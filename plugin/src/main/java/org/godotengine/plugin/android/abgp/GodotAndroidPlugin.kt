55f9c580546431399920b6c9e2ace8ce879012c6
🍁 <cardenasjairods@gmail.com>
2025-02-14 15:01:51 +0100

    initial setup

--- plugin/src/main/java/org/godotengine/plugin/android/abgp/GodotAndroidPlugin.kt
+++ plugin/src/main/java/org/godotengine/plugin/android/abgp/GodotAndroidPlugin.kt
@@ -1,15 +1,15 @@
-// TODO: Update to match your plugin's package name.
-package org.godotengine.plugin.android.template
+package org.godotengine.plugin.android.abgp
 
 import android.util.Log
 import android.widget.Toast
+import org.godotengine.godot.BuildConfig
 import org.godotengine.godot.Godot
 import org.godotengine.godot.plugin.GodotPlugin
 import org.godotengine.godot.plugin.UsedByGodot
 
 class GodotAndroidPlugin(godot: Godot): GodotPlugin(godot) {
 
-    override fun getPluginName() = BuildConfig.GODOT_PLUGIN_NAME
+    override fun getPluginName() = BuildConfig.LIBRARY_PACKAGE_NAME
 
     /**
      * Example showing how to declare a method that's used by Godot.
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,6 +15,5 @@ dependencyResolutionManagement {
     }
 }
 
-// TODO: Update project's name.
-rootProject.name = "GodotAndroidPluginTemplate"
+rootProject.name = "AndroidBluetoothGodotPlugin"
 include(":plugin")
